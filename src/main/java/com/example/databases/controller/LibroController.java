7204d0ac0da2f438847d5d4b0df27aaaddb82026
jestravis <jestravis@paradigmadigital.com>
2022-08-09 08:48:59 +0200

    Primeras validaciones

--- src/main/java/com/example/databases/controller/LibroController.java
+++ src/main/java/com/example/databases/controller/LibroController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -31,7 +32,7 @@ public class LibroController {
 
     @PostMapping("/libros")
     @Transactional
-    public ResponseEntity<Libro> altaLibro(@RequestBody Libro nuevo){
+    public ResponseEntity<Libro> altaLibro(@Valid @RequestBody Libro nuevo){
         Libro guardado=repository.findById(repository.save(nuevo).getId()).orElseThrow(NoSuchElementException::new);
         return new ResponseEntity<>(guardado,HttpStatus.CREATED);
     }
--- src/main/java/com/example/databases/model/Libro.java
+++ src/main/java/com/example/databases/model/Libro.java
@@ -8,6 +8,9 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
@@ -20,9 +23,14 @@ public class Libro {
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
     @Column
+    @NotNull
+    @NotBlank
     private String titulo;
     @Column
+    @NotNull
+    @NotBlank
     private String autor;
     @Column
+    @Positive(message = "El precio debe ser un número mayor que 0 (cero)")
     private Double precio;
 }
