1dbc486c8f5f9b74c95372b39bef99aab1447c4f
jestravis <jestravis@paradigmadigital.com>
2022-08-09 10:00:50 +0200

    Comienzos de test

--- src/test/java/com/example/databases/DatabasesApplicationTests.java
+++ src/test/java/com/example/databases/DatabasesApplicationTests.java
@@ -1,13 +1,50 @@
 package com.example.databases;
 
+import com.example.databases.model.Libro;
+import com.example.databases.repository.LibroRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Answers;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class DatabasesApplicationTests {
 
+	LibroRepository repository;
+	List<Libro> libros;
+
+	@BeforeEach
+	void setUp() {
+		repository=mock(LibroRepository.class);
+		libros=Arrays.asList(new Libro(Long.parseLong("1"),"Java 1","Goetz",90.0),
+				new Libro(Long.parseLong("2"),"Python","Furlong",99.0));
+	}
+
 	@Test
-	void contextLoads() {
+	void repositorySizeIsOk(){
+		when(repository.findAll()).thenReturn(libros);
+
+		assertThat(repository.findAll().size()).isEqualTo(2);
+	}
+	@Test
+	void repositorySizeIsOkWhenNewBookAdded(){
+		when(repository.findAll()).thenReturn(libros);
+
+		repository.save(new Libro(Long.parseLong("3"),"Scala","Hall",89.0));
+
+		assertThat(repository.findAll().size()).isEqualTo(3);
 	}
 
 }
