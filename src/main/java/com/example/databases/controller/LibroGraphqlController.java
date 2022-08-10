60de88092dd629fd54a57e39c4cd6670b88d9439
jestravis <jestravis@paradigmadigital.com>
2022-08-10 15:14:39 +0200

    Primera prueba mutation (KO)

--- src/main/java/com/example/databases/controller/LibroGraphqlController.java
+++ src/main/java/com/example/databases/controller/LibroGraphqlController.java
@@ -2,10 +2,12 @@ package com.example.databases.controller;
 
 import com.example.databases.model.Libro;
 import com.example.databases.repository.LibroRepository;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.Collection;
+import java.util.NoSuchElementException;
 
 @Controller
 public class LibroGraphqlController {
@@ -19,5 +21,15 @@ public class LibroGraphqlController {
     @QueryMapping
     public Collection<Libro> libros() {
         return repository.findAll();
+    }
+
+    @MutationMapping
+    public Libro addLibro(String titulo, String autor, Float precio){
+        Libro nuevo=new Libro();
+        nuevo.setAutor(autor);
+        nuevo.setTitulo(titulo);
+        nuevo.setPrecio(Double.parseDouble(precio.toString()));
+        repository.save(nuevo);
+        return repository.findById(nuevo.getId()).orElseThrow(()->new RuntimeException());
     }
 }
