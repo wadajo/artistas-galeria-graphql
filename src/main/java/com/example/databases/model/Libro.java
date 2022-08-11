2fd9a9c01865650100f14fc6a3b991c9d7c0ffd9
jestravis <jestravis@paradigmadigital.com>
2022-08-11 15:00:07 +0200

    Más pruebas mutations

--- src/main/java/com/example/databases/model/Libro.java
+++ src/main/java/com/example/databases/model/Libro.java
@@ -3,6 +3,7 @@ package com.example.databases.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
